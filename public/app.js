1c5c29390af30231051df6277f51d7979a192f59
Miguel Carmona <miguelrc1208@gmail.com>
2017-03-14 23:20:51 -0500

    done :D

--- public/app.js
+++ public/app.js
@@ -6,13 +6,30 @@ const mouse = {
 };
 
 const canvas  = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 const width   = window.innerWidth;
 const height  = window.innerHeight;
 const socket  = io.connect();
+let currentWidth = 5;
+let currentColor = '#00FA9A'
+
+const colorClickHandler = e => {
+  currentColor = e.target.dataset.color;
+  e.stopPropagation();
+}
+const buttonContainer = document.querySelector("#buttonContainer");
+buttonContainer.addEventListener("click", colorClickHandler);
+
+
+document.getElementById('selector').addEventListener('change', e => {
+  currentWidth = e.target.selectedIndex * 5
+  ctx.lineWidth = currentWidth
+})
 
 canvas.width = width;
 canvas.height = height;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
 
 canvas.onmousedown = e => mouse.click = true;
 canvas.onmouseup = e => mouse.click = false;
@@ -25,15 +42,19 @@ canvas.onmousemove = e => {
 
 socket.on('draw_line', data => {
   const line = data.line;
-  context.beginPath();
-  context.moveTo(line[0].x * width, line[0].y * height);
-  context.lineTo(line[1].x * width, line[1].y * height);
-  context.stroke();
+  ctx.beginPath();
+  ctx.strokeStyle = line[3];
+  ctx.lineWidth = line[2];
+  ctx.moveTo(line[0].x * width, line[0].y * height);
+  ctx.lineTo(line[1].x * width, line[1].y * height);
+  ctx.stroke();
+  ctx.strokeStyle = currentColor;
+  ctx.lineWidth = currentWidth;
 });
 
 const mainLoop = () => {
   if (mouse.click && mouse.move && mouse.pos_prev) {
-    socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+    socket.emit('draw_line', { line: [mouse.pos, mouse.pos_prev, currentWidth, currentColor] });
     mouse.move = false;
   }
   mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
